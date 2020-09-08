9768fea018b7ad1c852b8b9425de93e161b29e20
Arash Moghani <moghani.arash@gmail.com>
2020-09-08 18:31:55 +0430

    Correct proxy issue and get actual class

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
+/*
 open module com.github.wnameless.spring.routing.resolver {
   requires com.google.common;
   requires spring.core;
   requires spring.beans;
   requires transitive spring.web;
   requires transitive spring.context;
+    requires rt;
 
-  exports com.github.wnameless.spring.routing.resolver;
-}
+    exports com.github.wnameless.spring.routing.resolver;
+}*/
